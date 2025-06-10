8e40323ff2d9f898082c3bb4fa6a38bb3de947c7
azeth <azeth@azeth.net>
2025-06-10 22:54:18 +0200

    patches_10062025

--- bpi-r4-openwrt-builder.sh
+++ bpi-r4-openwrt-builder.sh
@@ -15,21 +15,22 @@
 rm -rf openwrt
 rm -rf mtk-openwrt-feeds
 
-git clone --branch openwrt-24.10 https://git.openwrt.org/openwrt/openwrt.git openwrt || true
+#git clone --branch openwrt-24.10 https://git.openwrt.org/openwrt/openwrt.git openwrt || true
++\cp -r ../openwrt openwrt
 #cd openwrt; git checkout 6706c8a6e358857e916737f5e0bb41d40003de26; cd -;		#fix compilation with GCC15
 cd openwrt; git checkout 0a21ab73121c7db7c9c92c7cbf2a7b8b586007a6; cd -;		#enable WiFi LED for Teltonika RUTX50#
 
 
-
-git clone  https://git01.mediatek.com/openwrt/feeds/mtk-openwrt-feeds || true
+#git clone  https://git01.mediatek.com/openwrt/feeds/mtk-openwrt-feeds || true
++\cp -r ../mtk-openwrt-feeds mtk-openwrt-feeds
 #cd mtk-openwrt-feeds; git checkout 11cc0ee3c62a119b16483d52dd63d634353804a8; cd -;	#Change nft flowoffload rule table to support ipv6 offload
 cd mtk-openwrt-feeds; git checkout aa3133f6f1ed93b99372b63636f75dbddd9547bd; cd -;	#Fix memory leak issue for the nft_flow_offload
 
 
 #echo "11cc0ee" > mtk-openwrt-feeds/autobuild/unified/feed_revision
 
-#\cp -r configs/defconfig mtk-openwrt-feeds/autobuild/unified/filogic/24.10/defconfig
-\cp -r configs/dbg_defconfig mtk-openwrt-feeds/autobuild/unified/filogic/24.10/defconfig
+\cp -r configs/defconfig mtk-openwrt-feeds/autobuild/unified/filogic/24.10/defconfig
+#\cp -r configs/dbg_defconfig mtk-openwrt-feeds/autobuild/unified/filogic/24.10/defconfig
 
 #Change Feeds Revision
 \cp -r my_files/w-feed_revision mtk-openwrt-feeds/autobuild/unified/feed_revision
@@ -38,22 +39,28 @@ cd mtk-openwrt-feeds; git checkout aa3133f6f1ed93b99372b63636f75dbddd9547bd; cd 
 rm -rf mtk-openwrt-feeds/24.10/patches-feeds/108-strongswan-add-uci-support.patch 
 
 ### wireless-regdb modification - this remove all regdb wireless countries restrictions
-#rm -rf openwrt/package/firmware/wireless-regdb/patches/*.*
-#rm -rf mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/files/package/firmware/wireless-regdb/patches/*.*
-#\cp -r my_files/500-tx_power.patch mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/files/package/firmware/wireless-regdb/patches
-#\cp -r my_files/regdb.Makefile openwrt/package/firmware/wireless-regdb/Makefile
+rm -rf openwrt/package/firmware/wireless-regdb/patches/*.*
+rm -rf mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/files/package/firmware/wireless-regdb/patches/*.*
+\cp -r my_files/500-tx_power.patch mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/files/package/firmware/wireless-regdb/patches
+\cp -r my_files/regdb.Makefile openwrt/package/firmware/wireless-regdb/Makefile
 
 ### adds a frequency match check to ensure the noise value corresponds to the interface's actual frequency for multiple radios under a single wiphy
-#\cp -r my_files/200-wozi-libiwinfo-fix_noise_reading_for_radios.patch openwrt/package/network/utils/iwinfo/patches
+\cp -r my_files/200-wozi-libiwinfo-fix_noise_reading_for_radios.patch openwrt/package/network/utils/iwinfo/patches
 
 ### tx_power patch - required for BE14 boards with defective eeprom flash
-#\cp -r my_files/99999_tx_power_check.patch mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/files/package/kernel/mt76/patches/
+\cp -r my_files/99999_tx_power_check.patch mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/files/package/kernel/mt76/patches/
 
 ### tx_power patch - by dan pawlik
-#\cp -r my_files/99999_tx_power_check_by_dan_pawlik.patch mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/files/package/kernel/mt76/patches/
+\cp -r my_files/99999_tx_power_check_by_dan_pawlik.patch mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/files/package/kernel/mt76/patches/
 
-### required & thermal zone 
+### required & thermal zone
 \cp -r my_files/1007-wozi-arch-arm64-dts-mt7988a-add-thermal-zone.patch mtk-openwrt-feeds/24.10/patches-base/
+
+### sfp 2.5G support patch
+\cp -r my_files/952-add-sfp-quirk.patch openwrt/target/linux/mediatek/patches-6.6/
+
+### change default ip address
+\cp -r my_files/image-config.in openwrt/package/base-files/image-config.in
 
 #sed -i 's/CONFIG_PACKAGE_perf=y/# CONFIG_PACKAGE_perf is not set/' mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/defconfig
 #sed -i 's/CONFIG_PACKAGE_perf=y/# CONFIG_PACKAGE_perf is not set/' mtk-openwrt-feeds/autobuild/autobuild_5.4_mac80211_release/mt7988_wifi7_mac80211_mlo/.config
